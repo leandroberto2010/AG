d0e4e131ffddedf9797ec4c8395c9c6166d530b6
leandroberto2010 <132699832+leandroberto2010@users.noreply.github.com>
2023-05-30 18:38:26 -0300

    Add files via upload

--- tp1/inicio.py
+++ tp1/inicio.py
@@ -1,50 +1,47 @@
-import funciones as fc
-import dataPrinter
+import logging
+import pandas as pd
+from funciones import iniciar_poblacion
+from funciones import evolucion
+from estadisticas import guardar_valores
+from estadisticas import grafica_conjunta
+
+# Configuracion del logging
+# Esto es para ver algunos resultados en un archivo (ver funcion mutacion)
+# En la consola solo mostramos maximo, minimo y promedio como pide el enunciado
+logging.basicConfig(filename='info.log', encoding='utf-8', level=logging.INFO, filemode="w") 
 
 poblacion = []
-poblacion_elitismo = []
-padres = []
-mejores = []
 maximos = []
 minimos = []
 promedios = []
-nueva_gen = []
-
-cross_rate = 0.75
-mut_rate = 0.05
-cantidad = 10
 ciclos = 100
-elitismo = False
-seleccion = 'ruleta'
+uri = "C:/Users/leand/Documents/facultad/algoritmos/tpFinal/tpfinalfinal/data.xlsx" #Directorio donde se va a guardar el archivo excel
 
-fc.iniciar_poblacion(poblacion, cantidad, x_min=0, x_max=(2**30)-1)
-fc.evaluar(poblacion)
-dataPrinter.mostrar_poblacion(poblacion, 0)
-print(f"############PROMEDIO GENERACION {0} ############ ")
-print("Promedio = ", fc.calc_promedio(poblacion))
-fc.guardar_valores(poblacion, maximos, minimos, promedios)
+iniciar_poblacion(poblacion, 
+                  cantidad = 10, 
+                  x_min = 0, 
+                  x_max = (2**30)-1)
+
+guardar_valores(poblacion, maximos, minimos, promedios)
 
 for i in range(ciclos):
-    if elitismo:
-        fc.salvar_mejores(mejores, poblacion)
-    nueva_gen.clear()
-    for j in range(int(len(poblacion)/2)):
-        if (seleccion == 'ruleta'):
-            fc.ruleta(poblacion, padres)
-        else:
-            fc.torneo(poblacion, padres)
-        dataPrinter.mostrar_padres(padres)
-        fc.crossover(nueva_gen, padres, cross_rate, mut_rate)
-    poblacion.clear()
-    poblacion = nueva_gen.copy()
-    if elitismo:
-        fc.insertar_mejores(mejores, poblacion)
-    fc.evaluar(poblacion)
-    dataPrinter.mostrar_poblacion(poblacion, i+1)
-    print(f"############PROMEDIO GENERACION {i+1} ############ ")
-    print("Promedio = ", fc.calc_promedio(poblacion))
-    fc.mostrar_mejor(poblacion)
-    fc.guardar_valores(poblacion, maximos, minimos, promedios)
+    poblacion = evolucion(metodo = "ruleta", 
+                          elitismo = False, 
+                          cross_rate = 0.75, 
+                          mut_rate = 0.30,
+                          poblacion = poblacion)
+    
+    guardar_valores(poblacion, maximos, minimos, promedios)
 
 
-fc.grafica_conjunta(minimos, promedios, maximos)
+valores = {
+    "Maximos": maximos,
+    "Minimos": minimos,
+    "Promedios": promedios
+}
+df = pd.DataFrame(valores)
+df.to_excel(uri, sheet_name="pag")
+
+print(df)
+
+grafica_conjunta(minimos, promedios, maximos)
