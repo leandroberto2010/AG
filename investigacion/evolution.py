b4f328a259702c114b662140bcb09ddd8d4efcdc
leandroberto2010 <132699832+leandroberto2010@users.noreply.github.com>
2023-09-01 16:01:38 -0300

    Add files via upload

--- investigacion/evolution.py
+++ investigacion/evolution.py
@@ -1,48 +1,51 @@
 from surface import Surface
 import stats
 import random
+import copy
 
-forbidden_locations = []
-CROSS_RATE = 0.75
-MUT_RATE = 0.05
-CYCLES = 200
+cross_rate = 0.3
+mut_rate = 0.05
+cycles = 500
 population = []
 padres = []
 next_generation = []
 maximos = []
 minimos = []
 promedios = []
+elite_population = []
+elitismo = False
+hijos = []
 
 def init(population_size = 10, height = 10, width = 10, turbines = 25):
     for _ in range(population_size):
         surface = Surface(height=height, width=width, turbines=turbines)
         population.append(surface)
 
-def evaluar():
+def evaluar(population):
     total = 0
     for ind in population:
-        total += ind.objective_function()
+        ind.power = ind.get_total_power()
+        total += ind.get_total_power()
     for ind in population:    
-        ind.fitness = ind.objective_function() / total
-    population.sort(key = lambda ind: (ind.fitness), reverse=True)
+        ind.fitness = ind.get_total_power() / total
+    population.sort(key = lambda ind: (ind.power), reverse=True)
 
 def torneo():
     contendientes = []
     while (len(padres)<2):
-        for _ in range(4):
+        for _ in range(10):
             posicion = random.randint(0, len(population)-1)
             contendientes.append(population[posicion])  
         padres.append(max(contendientes, key=lambda cont: cont.fitness))
-        (max(contendientes, key=lambda cont: cont.fitness))   
         contendientes.clear()
 
 def ruleta():
-    while (len(padres)<2):
+    while len(padres)<2:
         acum = 0   
         for ind in population:
-            peso = random.uniform(0, 1)
+            peso = random.random()
             acum += ind.fitness
-            if acum > peso:
+            if acum >= peso:
                 padres.append(ind)
                 break
 
@@ -52,48 +55,59 @@ def seleccion(metodo):
     if (metodo == "torneo"):
         return torneo()
     
-def crossover():
-    def obtener_cromosoma(padres):
-        return padres[0].surface[:cross_point] + padres[1].surface[cross_point:]
-    
-    if CROSS_RATE >= random.random():
-        cross_point = random.randint(0, padres[0].height)
-        child1 = Surface()
-        child2 = Surface()
+def crossover(padres, hijos):
+    hijos.clear()
+    cross_point = random.randint(0, padres[0].height-1)
+    child1=Surface()
+    child2=Surface()
 
-        child1.set_surface(obtener_cromosoma(padres))
-        child2.set_surface(obtener_cromosoma(padres[::-1]))
+    child1.set_surface(padres[0].surface[:cross_point] + padres[1].surface[cross_point:])
+    child2.set_surface(padres[1].surface[:cross_point] + padres[0].surface[cross_point:])
 
-        if (child1.turbine_count()>25 | child2.turbine_count()>25):
-            crossover()
-
-        mutacion(child1)
-        mutacion(child2)
-
-        next_generation.append(child1)
-        next_generation.append(child2)
+    if cross_rate >= random.random():
+        while child1.turbine_count() > 25:
+            child1.toggle_cell()
+        while child2.turbine_count() > 25:
+            child2.toggle_cell()
+        hijos.append(child1)
+        hijos.append(child2)
     else:
-        for padre in padres:
-            mutacion(padre)
-        next_generation.extend(padres)
+        hijos=[copy.deepcopy(p) for p in padres]
+    return hijos
+
 
 def mutacion(ind):
-    if MUT_RATE > random.random():
-        ind.toggle_cell()
-    if ind.turbine_count() > 25:
-        while ind.turbine_count()>25:
-            ind.toggle_cell()
+    if mut_rate > random.random():
+        ind.swap_cell()
+    return ind
 
-init(population_size=30, height=10, width=10)
-evaluar()
-for _ in range(CYCLES):
-    while (len(next_generation) < len(population)):
+def elite(population):
+    elite_population.clear()
+    for i in range(2):
+        elite_population.append(population.pop(0))
+
+init(population_size=50, height=10, width=10, turbines=25)
+evaluar(population)
+stats.save_data(population, maximos, minimos, promedios)
+for _ in range(cycles):
+    if elitismo:
+        elite(population)
+    while len(next_generation)<len(population):
         padres.clear()
         seleccion('torneo')
-        crossover()
-    population = next_generation
-    evaluar()
+        hijos = crossover(padres, hijos)
+        for h in hijos:
+            next_generation.append(h)
+    population.clear()
+    for ind in next_generation:
+        population.append(mutacion(ind))
+    next_generation.clear()
+    if elitismo:
+        for e in elite_population:
+            population.append(e)
+    evaluar(population)
     stats.save_data(population, maximos, minimos, promedios)
+
 
 for ind in population:
     ind.print_surface()
